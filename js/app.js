cf692bc947a155c422a15fd9ae392fb62607811e
maxkim77 <141907655+maxkim77@users.noreply.github.com>
2023-10-03 15:02:47 +0900

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,7 @@
+const $ = (selector) => document.querySelector(selector);
+
+// const makerBtn = $('#maker-btn');
+const chatBtn = $('#chat-btn');
+
+// makerBtn.addEventListener('click', () => location.href = './maker/')
+chatBtn.addEventListener('click', () => location.href = './another/index.html')
