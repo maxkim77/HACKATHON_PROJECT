c3c233e58535342236d73ebcc70825d94b9c3dc0
maxkim77 <141907655+maxkim77@users.noreply.github.com>
2023-10-14 17:56:13 +0900

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,22 +1,3 @@
-const $ = (selector) => document.querySelector(selector);
-
-// 메뉴 버튼
-//열기
-$(".menu-open-btn").addEventListener('click', () => {
-  document.body.classList.add("non-scroll");
-  $(".gnb").classList.add("opened");
-  $(".menu-close-btn").classList.add("opened");
-  $("#backdrop").style.display = 'block';
-  document.body.classList.add("non-scroll");
-});
-//닫기
-$('.menu-close-btn').addEventListener('click', () => {
-  $("#backdrop").style.display = 'none';
-  $(".gnb").classList.remove("opened");
-  $(".menu-close-btn").classList.remove("opened");
-  document.body.classList.remove("non-scroll");
-});
-
 // const makerBtn = $('#maker-btn');
 const chatBtn = $('#chat-btn');
 
---
+++ js/navi.js
@@ -0,0 +1,5 @@
+function navigateTo(url) {
+    window.location.href = window.location.origin + '/' + url;
+}
+
+// 홈화면에 있는 인덱스에서 다른 폴더에 있는 인덱스로 네비게이트 합니다.
